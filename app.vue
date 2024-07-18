e0388a1cb8f487f942607851202645b771cfd0aa
jasonlin1993 <ninjachen.lin@gmail.com>
2024-07-18 17:13:45 +0800

    Chroe Eslint + Prettier 環境建置

--- app.vue
+++ app.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>{{ title }}</h1>
   </div>
 </template>
+
+<script setup>
+const year = 2023;
+const title = `${year}  Nuxt.js 練習`;
+</script>
