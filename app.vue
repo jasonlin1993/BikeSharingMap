a2c37b455ab0ea97a31e42f3949fddddcd53d2a7
jasonlin1993 <ninjachen.lin@gmail.com>
2024-07-19 12:00:21 +0800

    Chroe: Tailwind Css 環境建置

--- app.vue
+++ app.vue
@@ -2,6 +2,12 @@
   <div>
     <h1>{{ title }}</h1>
     <TestComponent />
+    <div class="bg-white py-24">
+      <div class="flex flex-col items-center">
+        <h1 class="text-6xl font-semibold text-blue-600">2022 iThome</h1>
+        <p class="mt-4 text-9xl font-bold text-gray-900">鐵人賽</p>
+      </div>
+    </div>
   </div>
 </template>
 
