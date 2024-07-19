97fac63e3ad39f1fc83557eae3430d6103f9bf74
jasonlin1993 <ninjachen.lin@gmail.com>
2024-07-19 12:15:44 +0800

    Feat:新增分頁 新增尋找 YouBike、查詢自行車道、附近景點美食頁面路由

--- app.vue
+++ app.vue
@@ -1,19 +1,5 @@
 <template>
   <div>
-    <h1>{{ title }}</h1>
-    <TestComponent />
-    <div class="bg-white py-24">
-      <div class="flex flex-col items-center">
-        <h1 class="text-6xl font-semibold text-blue-600">2022 iThome</h1>
-        <p class="mt-4 text-9xl font-bold text-gray-900">鐵人賽</p>
-      </div>
-    </div>
+    <NuxtPage />
   </div>
 </template>
-
-<script setup lang="ts">
-import TestComponent from "./components/TestComponent.vue";
-
-const year = 2023;
-const title = `${year}  Nuxt.js 練習`;
-</script>
